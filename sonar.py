df543bdc213ffecdb373d72c66c2a37cd2504a25
Zichen <changzichen2000@gmail.com>
2021-12-20 10:50:13 -0500

    Save

--- sonar.py
+++ sonar.py
@@ -1,12 +1,20 @@
 # run infer by infer.py
 import sys
 import subprocess
+import requests
+from requests.auth import HTTPBasicAuth
 
-def printf(format, *args):
-    sys.stdout.write(format % args)
+# def printf(format, *args):
+#     sys.stdout.write(format % args)
 
 if __name__ == '__main__':
-    changeDirectory = "cd ./paho.mqtt.java"
-    runInfer="infer analyze -- mvn verify -DskipTests"
-    subprocess.call(runInfer.split(), cwd="./paho.mqtt.java")
-    sys.exit()
+    # changeDirectory = "cd ./paho.mqtt.java"
+    # runInfer="infer analyze -- mvn verify -DskipTests"
+    # subprocess.call(runInfer.split(), cwd="./paho.mqtt.java")
+    
+    r = requests.get('http://localhost:9000/api/issues/search?componentKeys=Eclipse-Ditto&types=VULNERABILITY',
+                        auth=HTTPBasicAuth('admin', 'changzichen251'))
+    print("before")
+    print(r.text)
+    print("helloworld")
+    # sys.exit()
