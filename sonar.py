dc9d2e06338258cbfe80d2e5afb85f4e328d2b6a
Zichen <changzichen2000@gmail.com>
2021-12-25 15:44:09 -0500

    Finish sonar json output

--- sonar.py
+++ sonar.py
@@ -2,19 +2,29 @@
 import sys
 import subprocess
 import requests
+import json
 from requests.auth import HTTPBasicAuth
 
 # def printf(format, *args):
 #     sys.stdout.write(format % args)
 
-if __name__ == '__main__':
-    # changeDirectory = "cd ./paho.mqtt.java"
-    # runInfer="infer analyze -- mvn verify -DskipTests"
-    # subprocess.call(runInfer.split(), cwd="./paho.mqtt.java")
-    
+projectKey = ''
+types = ''
+
+if __name__ == '__main__':  
+
+    # create a http request to retrieve the vulnerabilities of a project
     r = requests.get('http://localhost:9000/api/issues/search?componentKeys=Eclipse-Ditto&types=VULNERABILITY',
                         auth=HTTPBasicAuth('admin', 'changzichen251'))
-    print("before")
-    print(r.text)
-    print("helloworld")
+
+
+    print('================================================================')
+    data = json.loads(r.text)   # response text body is already a json, just load it
+    print(data)
+    print('================================================================')
+
+    # output the json file of the response
+    with open('data.json', 'w') as f:
+        json.dump(data, f, indent=4)
+
     # sys.exit()
