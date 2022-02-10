c3af62adc8fe9264c423677ef90964f85e4d0b06
Bokun Zhao <Sam2565876330@outlook.com>
2022-02-10 01:11:22 -0500

    plot functionality

--- Bokun's Script/SnykAutomation.py
+++ Bokun's Script/SnykAutomation.py
@@ -3,13 +3,37 @@ import subprocess
 import json
 import copy
 from builtins import print
+import matplotlib.pyplot as plt
 
 project_url = "https://github.com/eclipse/ditto.git"  # the web URL
 project_name = "ditto"  # the cloned folder name
 
+# statistics (each element in the list is for a specific release)
+versions = []  # list of all tags
+total = []  # list of total issues count
+source = []  # list of source code issues count
+test = []  # list of test code issues count
+fixed = []  # list of fixed issues count
+introduced = []  # list of new issues count
+pre = []  # list of old issues count
+unique = []  # list of encountered unique issues count
 
-def plot_graph ():
-    # TODO: implement this feature
+
+def plot_graph(dict):
+    # dict: ['fingerprint' : '["introVersion", "EndVersion", <dict>]']
+    # TODO: more sophisticated graph using data stored in dict
+    plt.title(project_name)
+    plt.plot(versions, total, '.-', label='total issues')
+    plt.plot(versions, source, '.-', label='source code issues')
+    plt.plot(versions, test, '.-', label='test code issues')
+    plt.plot(versions, fixed, '.-', label='fixed issues')
+    plt.plot(versions, introduced, '.-', label='new issues')
+    plt.plot(versions, pre, '.-', label='old issues')
+    plt.plot(versions, unique, '.-', label='unique issue count')
+    plt.yticks(list(range(0, max(max(total), max(unique))+1)))
+    plt.tick_params(axis='x', labelcolor='r', labelsize=10, rotation=90)
+    plt.legend()
+    plt.show()
     return
 
 
@@ -58,12 +82,19 @@ def parse_snyk_report(version, record, prev_no_of_issues):
     fix = prev_no_of_issues - preexist
     print("version: " + version + " summary:")
     print("\tno. of issues in raw report: " + str(len(sample_report)))
+    total.append(len(sample_report))
     print("\tno. of issues in source code report: " + str(len(source_code_issues)))
+    source.append(len(source_code_issues))
     print("\tno. of issues in test code report: " + str(len(test_code_issues)))  # SUCCESS!~
+    test.append(len(test_code_issues))
     print("\tno. of fixed issues: " + str(fix))
+    fixed.append(fix)
     print("\tno. of new issues: " + str(new))
+    introduced.append(new)
     print("\tno. of pre-existing issues: " + str(preexist))
+    pre.append(preexist)
     print("\tsize of recorded issues: " + str(len(record)) + '\n')
+    unique.append(len(record))
     return len(source_code_issues)  # source_code_issues, test_code_issues (or don't return anything)
 
 
@@ -71,7 +102,7 @@ def snyk_analysis(version_list, index):
     os.system("git reset --hard")
     os.system("git checkout master")
     current = version_list[index]
-    subprocess.run(['git', 'checkout', 'tags/' + current], stdout=subprocess.DEVNULL) # run command, supress CLI stdout
+    subprocess.run(['git', 'checkout', 'tags/' + current], stdout=subprocess.DEVNULL)  # run command, supress CLI stdout
     print("starting Snyk analysis on: " + current + "...")
     os.system("snyk code test --json > SnykOut.json")  # store report in a new file "SnykOut.json"
     os.chdir("..")
@@ -103,13 +134,13 @@ if __name__ == '__main__':
     print(versions)
     count = len(versions)
     print("total no. of releases (tags): " + str(count))
-    print("\n============================initialization complete!=============================\n")
-    input("press enter to continue:")
+    print("\n============================Initialization Complete!=============================\n")
+    # input("press enter to continue:")
 
     # Analysis
-    for i in range(0, count):
-        snyk_analysis(versions, i)
-    print("\n===============================Analysis finished!!===============================\n")
+    # for i in range(0, count):
+    #     snyk_analysis(versions, i)
+    print("\n===============================Analysis Finished!!===============================\n")
 
     # Parse
     os.chdir("../" + project_name + "versionReports")
@@ -120,3 +151,7 @@ if __name__ == '__main__':
     prev_issue_count = 0
     for i in range(0, count):  # index 35 is leshan-1.3.0
         prev_issue_count = parse_snyk_report(versions[i], track_lifecycle, prev_issue_count)
+    print("\n=============================Report Parse Finished!!=============================\n")
+
+    # Plot
+    plot_graph(track_lifecycle)
