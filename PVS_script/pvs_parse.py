a6b44456199d1eda12b93d5e2f66086680a7048a
Zichen <changzichen2000@gmail.com>
2022-03-06 10:16:37 -0500

    PVS script

--- PVS_script/pvs_parse.py
+++ PVS_script/pvs_parse.py
@@ -2,6 +2,7 @@
 
 # -------------------------------- import area --------------------------------
 from ctypes.wintypes import HACCEL
+from ensurepip import version
 import os
 import subprocess
 import re
@@ -44,7 +45,7 @@ tag: specific release version of the project
 path: path to the working directory
 Return: total count of issues, severity count, code count
 '''
-def pvs_num_of_issues_per_type(tag, path_to_project):
+def pvs_num_of_issues_per_type_one_release(tag, path_to_project, verbal=False):
     with open(path_to_project + tag + ".txt") as f:
         lines = f.read().splitlines()         # read in the lines without the newline char  
     # my severity count table    
@@ -54,7 +55,7 @@ def pvs_num_of_issues_per_type(tag, path_to_project):
           "Low": 0
     }
     # my code count table
-    code_table = {}
+    Vcode_table = {}
     # total issue counter
     total_issues = 0
     # then iterate through each line to parse:
@@ -79,21 +80,23 @@ def pvs_num_of_issues_per_type(tag, path_to_project):
         else: 
             severity_table["Low"] += issue_count
         # issue type count
-        if code in code_table:
-            code_table[code] += issue_count
+        if code in Vcode_table:
+            Vcode_table[code] += issue_count
         else: 
-            code_table[code] = issue_count
-    print(f"Total Issues Found: {total_issues}")
-    print(f"================================")
-    print(f"Severity Report: ")
-    print(f"  High has: {severity_table['High']}")
-    print(f"  Medium has: {severity_table['Medium']}")
-    print(f"  Low has: {severity_table['Low']}")
-    print(f"================================")
-    print(f"Issue Type Found: ")
-    print(code_table)
-    print("======= done =======")
-    return total_issues, severity_table, code_table
+            Vcode_table[code] = issue_count
+
+    if verbal:
+        print(f"Total Issues Found: {total_issues} at version: {tag}")
+        print(f"================================")
+        print(f"Severity Report: ")
+        print(f"  High has: {severity_table['High']}")
+        print(f"  Medium has: {severity_table['Medium']}")
+        print(f"  Low has: {severity_table['Low']}")
+        print(f"    =====                       ")
+        print(f"Issue Type Found: ")
+        print(Vcode_table)
+        print("============= done ==============")
+    return total_issues, severity_table, Vcode_table
 
 
 # function to start the pvs analyzer
@@ -111,18 +114,20 @@ path_to_project: path to the working directory
 return: the list of tags
 '''
 def cal_changed_lines(path_to_project):
-    str = subprocess.check_output(['git', 'tag'], cwd=path_to_project)
+    str = subprocess.check_output(['git', 'tag', '--sort=-creatordate'], cwd=path_to_project)
     tag_lst = str.decode("utf-8").split("\n")[:-1]         ## decode the bytes into string and drop last term
     
+    tag_lst = list(reversed(tag_lst))
+
     changed_lst = ['null']                                 ## have a null at the first tag since it has no version to compare with
     for i in range(len(tag_lst) - 1):
         str = subprocess.check_output(['git', 'diff', '--shortstat', tag_lst[i], tag_lst[i+1]], cwd=path_to_project)
         changed_lst.append(str.decode('utf8').split("\n")[0])
 
     ## then parse the changed lst:
-    file_change = ['null']          ## change of files
-    add_change = ['null']           ## addition of lines
-    del_change = ['null']           ## deletion of lines
+    file_change = [1]          ## change of files
+    add_change = [1]           ## addition of lines
+    del_change = [1]           ## deletion of lines
     for i in range(1, len(changed_lst)):
         tmp = [int(s) for s in changed_lst[i].split() if s.isdigit()]
         file_change.append(tmp[0])
@@ -145,64 +150,108 @@ def pvs_plot_comparison(version_list, path_to_project, changed_lst=None, file_ch
     severity_m_list = []
     severity_l_list = []
     code_list = []
+    pretty_version_list = []
+    # count the differences between releases:
+    last_total = 0
+    total_diff = []
 
     ## count issues per version
     for v in version_list:
-        total, severity, code = pvs_num_of_issues_per_type(v, path_to_project)
+        total, severity, code = pvs_num_of_issues_per_type_one_release(v, path_to_project)
+        total_diff.append(total - last_total)
+        last_total = total
         total_list.append(total)
-        print(f"type of severity value: {type(severity['High'])}")
         severity_h_list.append(severity['High'])
         severity_m_list.append(severity['Medium'])
         severity_l_list.append(severity['Low'])
         code_list.append(code)
-    
+        pretty_version_list.append(v[7:])
 
-    newIssues_per_tag = np.array(total_list) / np.array(add_change)
-    print(newIssues_per_tag)
+    # the first element of issue_difference is 0
+    total_diff[0] = 0
 
-    ## plotting
-    _, plots = plt.subplots(2, 2)
+    # calculate a list of new issues:
+    new_issues = []
+    for i in range(len(total_diff)):
+        if total_diff[i] < 0:
+            new_issues.append(0)
+        else:
+            new_issues.append(total_diff[i])
+
+    # calculate net lines changed(dirivative)
+    net_lines_changed = np.array(add_change)-np.array(del_change)
+
+    # calculate differential of issues/net_line_changed
+    issue_diff_per_tag = np.array(total_diff) / np.array(net_lines_changed)
+
+    # calculate new issues per added lines:
+    new_issues_per_new_lines = np.array(new_issues) / np.array(add_change)
+
+    # calculate the new issues per files changed
+    issue_diff_per_file_changed = np.array(new_issues) / np.array(file_change)
+
+    ## plotting-1
+    _, plots_total = plt.subplots(2, 2)
     # plot: total-version
-    plots[0,0].set_title('Total issues -- version', size=8)
-    plots[0,0].plot(version_list, total_list, '.-', label='total issues')
-    
+    plots_total[0,0].set_title('Total issues -- version', size=16)
+    plots_total[0,0].plot(pretty_version_list, total_list, '.-', label='total issues')
     # plot: severity High-version
-    plots[0,1].set_title('Severity issues VS version', size=8)
-    plots[0,1].plot(version_list, severity_h_list, '.-', label='high')
-    plots[0,1].plot(version_list, severity_m_list, '.-', label='medium')
-    plots[0,1].plot(version_list, severity_l_list, '.-', label='low')
-    plots[0,1].legend()
+    plots_total[1,0].set_title('Severity issues VS version', size=16)
+    plots_total[1,0].plot(pretty_version_list, severity_h_list, '.-', label='high')
+    plots_total[1,0].plot(pretty_version_list, severity_m_list, '.-', label='medium')
+    plots_total[1,0].plot(pretty_version_list, severity_l_list, '.-', label='low')
+    plots_total[1,0].legend()
+    # plot: total-difference between releases
+    plots_total[0,1].set_title('Total differences -- version', size=16)
+    plots_total[0,1].plot(pretty_version_list, total_diff, '.-', label='total differences')
+    plt.setp(plots_total[0,0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_total[0,1].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_total[1,0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.show() # this is a blocking call; kill the plotting window to continue execution
 
-    # plots[1,1].set_title('Issue Code', size=8)
-    # plots[1,1].imshow( BlurImage(A, image), cmap, vmin=0, vmax=1)
-
-    # plots[1,0].set_title('deblurred image - computed', size=8)
-    # plots[1,0].imshow( DeblurImage(A, blurred_image), cmap, vmin=0, vmax=1)
+    
+    ## plotting-2
+    _, plots_new = plt.subplots(2,2)
+    plots_new[0,0].set_title('Lines deleted per release', size=16)
+    plots_new[0,0].plot(pretty_version_list, del_change, '.-', label='lines deleted')
+    # plot: new lines added per release
+    plots_new[0,1].set_title('New lines added per release', size=16)
+    plots_new[0,1].plot(pretty_version_list, add_change, '.-', label='new lines added')
+    # plot: new lines added per release
+    plots_new[1,0].set_title('Net lines changed per release', size=16)
+    plots_new[1,0].plot(pretty_version_list, net_lines_changed, '.-', label='new lines added')
+    # plot: new lines added per release
+    plots_new[1,1].set_title('Files changed per release', size=16)
+    plots_new[1,1].plot(pretty_version_list, file_change, '.-', label='files')
+    plt.setp(plots_new[0,0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_new[0,1].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_new[1,0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.xticks(rotation=90)
     plt.show() # this is a blocking call; kill the plotting window to continue execution
 
 
+    _, plots_result1 = plt.subplots(1,2)
+    plots_result1[0].set_title('Differential of issues/1000net_line_changed', size=16)
+    plots_result1[0].plot(pretty_version_list, issue_diff_per_tag*1000, '.-', label='differential of issues/net_line_changed') 
+    plots_result1[1].set_title('New issues/1000lines added', size=16)
+    plots_result1[1].plot(pretty_version_list, new_issues_per_new_lines*1000, '.-', label='New issues/lines added') 
+    plt.setp(plots_result1[0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_result1[1].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.show() # this is a blocking call; kill the plotting window to continue execution
+
+
+    _, plots_result2 = plt.subplots(1,2)
+    plots_result2[0].set_title('New issues/files added', size=16)
+    plots_result2[0].plot(pretty_version_list, issue_diff_per_file_changed, '.-', label='differential of issues/net_line_changed') 
+    # plots_result2[1].set_title('New issues/1000lines added', size=16)
+    # plots_result2[1].plot(pretty_version_list, new_issues_per_new_lines*1000, '.-', label='New issues/lines added') 
+    plt.setp(plots_result2[0].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.setp(plots_result2[1].xaxis.get_majorticklabels(),rotation=90,horizontalalignment='right')
+    plt.show() # this is a blocking call; kill the plotting window to continue executio
 
 if __name__ == '__main__': 
-    ## ======================== please keep these for now ========================
-    # version_list = ['leshan-1.0.1', 'leshan-1.0.2', 'leshan-1.1.0', 'leshan-1.2.0', 'leshan-1.3.0', 'leshan-1.3.1', 
-    # 'leshan-1.3.2']
-
-    # version_list = ['leshan-2.0.0-M1', 'leshan-2.0.0-M2', 'leshan-2.0.0-M3', 'leshan-2.0.0-M4', 'leshan-2.0.0-M5', 
-    #     'leshan-2.0.0-M5', 'leshan-2.0.0-M6']
-
-    # version_list = ['leshan1.0.0_M7', 'leshan1.0.0_M8', 'leshan1.0.0_M9', 'leshan1.0.0_RC1', 'leshan1.0.0_RC2', 
-    #     'leshan-1.0.1', 'leshan-1.0.2', 'leshan-1.1.0', 'leshan-1.2.0', 'leshan-1.3.0', 'leshan-1.3.1', 'leshan-1.3.2']
-    ## ===========================================================================
-
-
-
-    # version_list = ['2.1.0', '2.1.1', '2.1.2', '2.1.3', '2.2.0', '2.2.1', '2.2.2', '2.3.0', '2.3.1',
-    #     '2.3.2']
-
     print("\n================== PVS Analysis Result ==================")
 
     version_list, changed_lst, file_change, add_change, del_change = cal_changed_lines("./leshan")
- 
-    pvs_plot_comparison(version_list, "pvs_infer/", changed_lst, add_change=add_change, del_change=del_change)
 
-    print("test")
+    pvs_plot_comparison(version_list, "pvs_infer/", changed_lst, file_change=file_change, add_change=add_change, del_change=del_change)      ## the scaling is not very
